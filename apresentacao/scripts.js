07d8c1c42a812dcbf1ae97e0d381938d5e1f200c
Victor Soffi <victorsoffi@gmail.com>
2024-03-15 12:12:27 -0300

    first commit

---
+++ apresentacao/scripts.js
@@ -0,0 +1,12 @@
+function mostrarMensagem() {
+    alert("Deseja recarregar a pagina?");
+    location.reload ();
+}
+
+function redirecionarlin() {
+    window.location.href = "https://www.linkedin.com/in/victor-soffi-11aaa2211/";
+}
+
+function redirecionargit() {
+    window.location.href = "https://github.com/Kuroshi7?tab=repositories";
+}
